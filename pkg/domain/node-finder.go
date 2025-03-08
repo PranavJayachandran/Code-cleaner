10443be0f9904d84f97d6cd0b9e5c11252c6a5f7
pranavj <pranavj@carestack.com>
2025-03-08 12:20:52 +0530

    fixed issues related to path and escape

--- pkg/domain/node-finder.go
+++ pkg/domain/node-finder.go
@@ -2,16 +2,18 @@ package domain
 
 import (
 	"errors"
+	"fmt"
 	"path/filepath"
 	"strings"
 )
 
 func NodeFinder(line string, filePath string, lineNumber int) (Node, error) {
-  if isTrue, val, isExport := isInterface(line); isTrue {
+	fmt.Println(filePath)
+	if isTrue, val, isExport := isInterface(line); isTrue {
 		node := Node{}
-	  ext := filepath.Ext(filePath)         
-    filePath = strings.TrimSuffix(filePath, ext) 		
-    node.CreateNode(Interface, val, filePath, []string{}, lineNumber, isExport)
+		ext := filepath.Ext(filePath)
+		filePath = strings.TrimSuffix(filePath, ext)
+		node.CreateNode(Interface, val, filePath, []string{}, lineNumber, isExport)
 		return node, nil
 	}
 	return Node{}, errors.New("No matching node for this type")
@@ -20,7 +22,7 @@ func NodeFinder(line string, filePath string, lineNumber int) (Node, error) {
 func isInterface(line string) (bool, string, bool) {
 	elements := strings.Split(line, " ")
 	if elements[0] == "export" && elements[1] == "interface" {
-		return true, strings.TrimRight(strings.TrimRight(elements[2], "{"), " "), true;
-  }
-  return false, "", false
+		return true, strings.TrimRight(strings.TrimSpace(elements[2]), "{"), true
+	}
+	return false, "", false
 }
