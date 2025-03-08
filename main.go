b8d9c5f95a408b9f5fb7f09cd996d065b69fbc91
pranavj <pranavj@carestack.com>
2025-03-08 11:31:52 +0530

    fix

--- main.go
+++ main.go
@@ -8,20 +8,21 @@ import (
 )
 
 func main() {
-  startDir, _ := os.Getwd()
-  if len(os.Args) > 1 {
-    startDir = os.Args[1]
-  }
-  fmt.Printf("Starting the walk from %s\n", startDir);
-  file.InitialiseWalks(startDir)
-  startDir = filepath.Join(startDir, "src")
-  err := filepath.Walk(startDir, file.DeclarationWalk);
-  if err != nil{
-    fmt.Println(err)
-  }
-  err = filepath.Walk(startDir, file.ImportWalk);
-  if err != nil {
-    fmt.Println(err)
-  }
-  file.Result()
+	startDir, _ := os.Getwd()
+	if len(os.Args) > 1 {
+		startDir = os.Args[1]
+	}
+	fmt.Printf("Starting the walk from %s\n", startDir)
+	file.InitialiseWalks(startDir)
+	file.StartPath = startDir
+	startDir = filepath.Join(startDir, "src")
+	err := filepath.Walk(startDir, file.DeclarationWalk)
+	if err != nil {
+		fmt.Println(err)
+	}
+	err = filepath.Walk(startDir, file.ImportWalk)
+	if err != nil {
+		fmt.Println(err)
+	}
+	file.Result()
 }
